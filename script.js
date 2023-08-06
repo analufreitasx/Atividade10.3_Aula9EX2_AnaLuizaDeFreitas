64b3c0fe147eff2e7cd890f28416e11b7dd2292c
Ana Luiza de Freitas <132159788+analufreitasx@users.noreply.github.com>
2023-08-06 01:23:31 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+function SomaEMédia (soma, quantnum){
+    return soma /num; 
+}
+var vz = 1, soma = 0
+const vezes = parseInt (prompt('Este site executa o cálculo da soma e média dos números digitados.\n Insira quantos números deseja digitar: '))
+while(vz <= vezes){
+
+    var num = parseInt (prompt('Digite um número:'))
+    soma = soma + num
+    vz ++
+}
+
+alert("A soma dos números digitados é igual a " + soma + ". \nA média dos números digitados é igual a " + SomaEMédia (soma, vezes) + ".")
